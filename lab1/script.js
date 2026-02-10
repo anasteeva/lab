18c65621554e782c1e3d67afb99bc492750ea824
ykywy <anasteeva@icloud.com>
2026-02-10 21:44:25 +0200

    task2 cond & loops

--- lab1/script.js
+++ lab1/script.js
@@ -1,7 +1,12 @@
-let name = "Anastasia";
-let birthYear = 2006;
-let isStudent = true;
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+  console.log("Отлично!");
+} else if (score >= 70) {
+  console.log("Хорошо");
+} else {
+  console.log("Можно лучше!");
+}
 
-console.log(name);
-console.log(birthYear);
-console.log(isStudent);
+for (let i = 1; i <= 5; i++) {
+  console.log(`Итерация: ${i}`);
+}
